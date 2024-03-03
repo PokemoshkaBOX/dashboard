652e93ec19900702cc5ef1474d3c7e7c998029ee
Kekich64 <lenhik.ilin1@mail.ru>
2024-03-03 20:16:32 +0300

    my change

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,6 +1,6 @@
 const Router = require('express')
 const router = new Router()
-const userController = require('../controllers/userController')
+const userController = require('../Controllers/userController')
 const authMiddleware = require('../middleware/authMiddleware')
 
 router.post('/registration', userController.registration)
