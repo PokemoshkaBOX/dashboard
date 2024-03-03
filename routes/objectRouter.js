624b4516a5f90e71299d885e0162308383d7f903
PokemoshkaBOX <131866226+PokemoshkaBOX@users.noreply.github.com>
2024-03-03 21:16:04 +0300

    Update objectRouter.js

--- routes/objectRouter.js
+++ routes/objectRouter.js
@@ -1,6 +1,6 @@
 const Router = require('express')
 const router = new Router()
-const objectController = require('../Controllers/objectController')//импортируем контроллер
+const objectController = require('../controllers/objectController')//импортируем контроллер
 
 router.get('/', objectController.getAll)
 
