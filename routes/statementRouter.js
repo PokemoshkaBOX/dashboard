9de9835f68dd735df2eecff88b61fc252663133b
PokemoshkaBOX <131866226+PokemoshkaBOX@users.noreply.github.com>
2024-03-03 21:16:30 +0300

    Update statementRouter.js

--- routes/statementRouter.js
+++ routes/statementRouter.js
@@ -1,6 +1,6 @@
 const Router = require('express')
 const router = new Router()
-const statementController = require('../Controllers/statementController')//импортируем контроллер
+const statementController = require('../controllers/statementController')//импортируем контроллер
 
 router.get('/', statementController.getAll)
 
